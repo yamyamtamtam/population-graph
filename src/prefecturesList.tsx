51c8e8e8ed6288ac8d00e06538da4c274197576f
山崎　圭祐 <7623@NT2020121401.local>
2022-07-11 11:45:26 +0900

    これだと非同期通信より前に初期値でreturnしてしまうが、elseのreturn省くとlintがエラー出す

--- src/prefecturesList.tsx
+++ src/prefecturesList.tsx
@@ -1,20 +1,38 @@
 import React, { useState, useEffect } from "react";
 import PrefectureListGet from "./prefecturesListGet";
 
+type moduleReturn = {
+  datas: prefType[];
+  status: boolean;
+}
 type prefType = {
   prefCode: number;
   prefName: string;
 };
 const PrefecturesList = () => {
-  const [datas, setDatas] = useState<prefType[]>([]);
+  const [dataList, setdataList] = useState<moduleReturn>({datas: [], status: false});
   const apiUrl = "https://opendata.resas-portal.go.jp/api/v1/prefectures";
   useEffect(() => {
-    setDatas(PrefectureListGet(apiUrl));
+    setdataList(PrefectureListGet(apiUrl));
   }, []);
+  console.table(dataList);
 
   return (
     <div>
       <h2>都道府県</h2>
+      <ul>
+        {
+          (()=>{
+            if(dataList.status){
+              return(
+                <ul>
+                  <li>{dataList.datas[0].prefName}</li>
+                </ul>)
+            }
+              return(<p>データが取得できませんでした。</p>)
+          })()
+        }
+      </ul>
     </div>
   );
 };
--- src/prefecturesListGet.tsx
+++ src/prefecturesListGet.tsx
@@ -1,32 +1,42 @@
 import React from "react";
 import axios, { AxiosResponse, AxiosError } from "axios";
 
-type RESULT = {
+type apiReslut = {
   message: string;
   result: prefType[];
+};
+type moduleReturn = {
+  datas: prefType[];
+  status: boolean;
 };
 type prefType = {
   prefCode: number;
   prefName: string;
 };
 
-let returnArray: prefType[] = [];
-
-const PrefecturesListGet = (url: string): prefType[] => {
+const PrefecturesListGet = (url: string): moduleReturn => {
+  const returnArray: moduleReturn = {
+    datas: [{prefCode:1,prefName:"初期値"}],
+    status: true,
+  };
   const apiKey = process.env.REACT_APP_API_KEY;
   if (typeof apiKey === "undefined") {
     return returnArray;
   }
   axios
     .get(url, { headers: { "X-API-KEY": apiKey } })
-    .then((res: AxiosResponse<RESULT>) => {
-      returnArray = res.data.result;
+    .then((res: AxiosResponse<apiReslut>) => {
+      returnArray.datas = res.data.result;
+      returnArray.status = true;
+      console.log('axios ok');
       return returnArray;
     })
     .catch((e: AxiosError) => {
+      returnArray.status = true;
+      console.log('axios ng');
       return returnArray;
     });
-  return returnArray;
+    console.log('axios first');
 };
 
 export default PrefecturesListGet;
